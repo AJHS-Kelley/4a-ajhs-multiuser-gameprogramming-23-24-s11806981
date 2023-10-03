3eac117aeaf52bca2357a51debed947a2a2cd914
s11806981 <90473201+s11806981@users.noreply.github.com>
2023-10-03 14:09:22 -0400

    02_Collections

---
+++ computer_science_exercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace computer_science_exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
