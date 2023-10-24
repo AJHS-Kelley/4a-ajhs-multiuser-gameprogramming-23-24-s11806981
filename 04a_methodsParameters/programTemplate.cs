73de80c54f7238641217e5255f0b95246505a083
s11806981 <90473201+s11806981@users.noreply.github.com>
2023-10-24 14:09:01 -0400

    v0.01

---
+++ 04a_methodsParameters/programTemplate.cs
@@ -0,0 +1,20 @@
+// Ashley Wright, Program Template, v0.01
+using System;
+
+namespace MethodsParameters
+{
+    class MethodsParameters
+    {
+        // METHOD -- name block of code, can be used over and over.
+        // All methods have a SIGNATURE that defines their name, parameters, and output.
+        // Example Signature
+        static void MyMethod()
+        {
+            Console.WriteLine("cookies for breakfast.\n");
+        }
+        static void Main(string[] args)
+        {
+            MyMethod();
+        }
+    }
+}
