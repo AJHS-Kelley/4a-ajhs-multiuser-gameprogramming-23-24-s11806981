27a6d8ae58012c7b3ef82f35a6b77c142654f30d
s11806981 <90473201+s11806981@users.noreply.github.com>
2023-10-24 14:41:04 -0400

    v0.02

--- 04a_methodsParameters/programTemplate.cs
+++ 04a_methodsParameters/programTemplate.cs
@@ -1,4 +1,4 @@
-// Ashley Wright, Program Template, v0.01
+// Ashley Wright, Program Template, v0.02
 using System;
 
 namespace MethodsParameters
@@ -12,9 +12,33 @@ namespace MethodsParameters
         {
             Console.WriteLine("cookies for breakfast.\n");
         }
+        // static -- This method belongs to the current class, it is NOT an object.
+        //void -- This method has no return value.
+
+        static int DoubleUp()
+        {
+            int sum = 0;
+            Console.WriteLine("This method will double a number and return it.\n");
+            Console.WriteLine("Please enter a number on the next line.\n");
+            sum = System.Convert.ToInt32(Console.ReadLine());
+            sum *= 2;
+            Console.WriteLine(sum);
+            return sum; 
+        }
+
+        // Methods with Parameters
+        static void MakePancakes(int num)
+        {
+            for (int i = 0; i < num; i++) 
+            {
+                Console.WriteLine("One fluffy pancake coming up!\n");
+            }
+        }
         static void Main(string[] args)
         {
-            MyMethod();
+            //MyMethod();
+            //DoubleUp();
+            MakePancakes(2);
         }
     }
 }
